1590eb29a41327667668800f33fd14b6900b94c4
Axel Rindle <axel.rindle@gmx.de>
2018-06-16 20:09:56 +0200

    Add PocketInventory.kt and InventoryUtils.kt

---
+++ src/main/kotlin/de/axelrindle/pocketknife/PocketInventory.kt
@@ -0,0 +1,85 @@
+package de.axelrindle.pocketknife
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * A helper class for easy management of inventories.
+ */
+class PocketInventory(
+        plugin: JavaPlugin,
+        val name: String,
+        val size: Int
+) {
+
+    private val listener: PocketInventory.ClickListener
+    private val itemList: HashMap<Int, ItemStack> = HashMap()
+    private val clickListeners: HashMap<Int, (event: InventoryClickEvent) -> Unit> = HashMap()
+
+    init {
+        // minimum and maximum size
+        if (size < 1 || size > 6)
+            throw IllegalArgumentException("size must be between 1 and 6, got '$size'!")
+
+        // register event listener
+        listener = ClickListener(this)
+        Bukkit.getPluginManager().registerEvents(listener, plugin)
+    }
+
+    /**
+     * Adds a new item to the inventory.
+     *
+     * @param position The position of the item in the inventory.
+     * @param stack The [ItemStack] to add.
+     * @param handler A click listener called when this item is clicked. Can be
+     */
+    fun setItem(position: Int, stack: ItemStack, handler: ((event: InventoryClickEvent) -> Unit)? = null) {
+        // validate position
+        if (position < 0 || position > 53)
+            throw IllegalArgumentException("position must be between 0 and 53, got '$position'!")
+
+        itemList[position] = stack
+        if (handler == null) return
+        clickListeners[position] = handler
+    }
+
+    /**
+     * Removes an item.
+     *
+     * @param position The index of the item to remove.
+     */
+    fun removeItem(position: Int) {
+        itemList.remove(position)
+        clickListeners.remove(position)
+    }
+
+    /**
+     * Open this inventory for a player.
+     *
+     * @param player The [Player] who should see the inventory.
+     */
+    fun open(player: Player) {
+        val inv = Bukkit.createInventory(player, size * 9, name)
+        itemList.forEach(inv::setItem)
+        player.openInventory(inv)
+    }
+
+    /**
+     * A [Listener] which listens for inventory events.
+     */
+    private class ClickListener(
+            private val pocketInventory: PocketInventory
+    ) : Listener {
+
+        @EventHandler
+        fun onItemClicked(e: InventoryClickEvent) {
+            e.isCancelled = true
+            pocketInventory.clickListeners[e.slot]?.invoke(e)
+        }
+    }
+}
---
+++ src/main/kotlin/de/axelrindle/pocketknife/util/InventoryUtils.kt
@@ -0,0 +1,46 @@
+package de.axelrindle.pocketknife.util
+
+import de.axelrindle.pocketknife.PocketInventory
+
+/**
+ * Utility methods for use with the [PocketInventory] class.
+ */
+@Suppress("unused")
+object InventoryUtils {
+
+    private val INDEX_MATRIX = Array(9) { Array(6) { it } }
+
+    init {
+        var i = 0
+        for (y in 0..5)
+            for (x in 0..8)
+                INDEX_MATRIX[x][y] = i++
+    }
+
+    /**
+     * Returns an inventory index from the given matrix.
+     *
+     * An inventory index matrix looks like this:
+     * ```
+     * /  |  1  2  3  4  5  6  7  8  9
+     * -  |  -- -- -- -- -- -- -- -- --
+     * 1  |  0  1  2  3  4  5  6  7  8
+     * 2  |  9  10 11 12 13 14 15 16 17
+     * 3  |  18 19 20 21 22 23 24 25 26
+     * 4  |  27 28 29 30 31 32 33 34 35
+     * 5  |  36 37 38 39 40 41 42 43 44
+     * 6  |  45 46 47 48 49 50 51 52 53
+     * ```
+     */
+    fun getIndex(column: Int, row: Int): Int {
+        // validate column index
+        if (column < 1 || column > 9)
+            throw IllegalArgumentException("column index must be between 1 and 9, got '$column'!")
+
+        // validate row index
+        if (row < 1 || row > 6)
+            throw IllegalArgumentException("row index must be between 1 and 6, got '$row'!")
+
+        return INDEX_MATRIX[column-1][row-1]
+    }
+}
