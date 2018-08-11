5476bd5eb97a492fa739b8b689d81dc1d92fc8b2
Axel Rindle <axel.rindle@gmx.de>
2018-08-11 11:28:49 +0200

    Create a plugin file to avoid duplicate class errors

---
+++ src/main/kotlin/de/axelrindle/pocketknife/PocketKnifePlugin.kt
@@ -0,0 +1,10 @@
+package de.axelrindle.pocketknife
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PocketKnifePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        logger.info("PocketKnife v${description.version} loaded.")
+    }
+}
