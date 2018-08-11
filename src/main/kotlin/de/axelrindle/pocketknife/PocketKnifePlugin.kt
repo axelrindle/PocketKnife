7b8cf664951a760cd744c84fef64821ad5faf1dd
Axel Rindle <axel.rindle@gmx.de>
2018-08-11 12:51:34 +0200

    Add disabled message

--- src/main/kotlin/de/axelrindle/pocketknife/PocketKnifePlugin.kt
+++ src/main/kotlin/de/axelrindle/pocketknife/PocketKnifePlugin.kt
@@ -5,6 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin
 class PocketKnifePlugin : JavaPlugin() {
 
     override fun onEnable() {
-        logger.info("PocketKnife v${description.version} loaded.")
+        logger.info("PocketKnife v${description.version} enabled.")
+    }
+
+    override fun onDisable() {
+        logger.info("PocketKnife v${description.version} disabled.")
     }
 }
