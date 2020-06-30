5cd3a7798a347736391ced0a41407124e736d4c6
Axel Rindle <mail@axelrindle.de>
2020-06-30 13:41:14 +0200

    Update PocketLangTest

--- api/src/test/kotlin/PocketLangTest.kt
+++ api/src/test/kotlin/PocketLangTest.kt
@@ -5,10 +5,7 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.endWith
-import java.lang.IllegalArgumentException
-import java.lang.IllegalStateException
 
 class PocketLangTest : ShouldSpec({
 
@@ -17,37 +14,47 @@ class PocketLangTest : ShouldSpec({
     val pocketConfig = PocketConfig(mockedPlugin)
     val pocketLang = PocketLang(mockedPlugin, pocketConfig)
 
-    should("PocketLang.getDefaultConfig() should return null before init") {
-        pocketLang.getDefaultConfig() shouldBe null
-    }
-
-    should("PocketLang.getLocaleConfig() should return null before init") {
-        pocketLang.getLocaleConfig() shouldBe null
-    }
-
-    should("PocketLang.init() fail without adding languages") {
-        val exception = shouldThrow<IllegalStateException> {
-            pocketLang.init()
+    context("config getters") {
+        should("return null before init") {
+            pocketLang.getDefaultConfig() shouldBe null
+            pocketLang.getLocaleConfig() shouldBe null
         }
-        exception.message shouldBe "No languages were registered!"
     }
 
-    should("PocketLang.init() succeed") {
-        pocketLang.addLanguages("en", "de")
-
-        val exception = shouldThrow<IllegalArgumentException> {
-            pocketLang.addLanguage("en")
+    context("init") {
+        should("fail without adding languages") {
+            val exception = shouldThrow<IllegalStateException> {
+                pocketLang.init()
+            }
+            exception.message shouldBe "No languages were registered!"
         }
-        exception.message should endWith("is already registered!")
 
-        pocketLang.init()
+        should("succeed with languages added") {
+            pocketLang.addLanguages("en", "de")
+
+            val exception = shouldThrow<IllegalArgumentException> {
+                pocketLang.addLanguage("en")
+            }
+            exception.message should endWith("is already registered!")
+
+            pocketLang.init("de")
+        }
+
+        should("correctly override default language") {
+            pocketConfig.access("localization")!!.apply {
+                get("UseLanguage") shouldBe "de"
+                get("DefaultLanguage") shouldBe "de"
+            }
+        }
     }
 
-    should("PocketLang.getDefaultConfig() should NOT return null AFTER init") {
-        pocketLang.getDefaultConfig() shouldNotBe null
-    }
+    context("localize") {
+        should("return null when no matching key is found") {
+            pocketLang.localize("unknownMessage") shouldBe null
+        }
 
-    should("PocketLang.getLocaleConfig() should NOT return null AFTER init") {
-        pocketLang.getLocaleConfig() shouldNotBe null
+        should("return the localized string") {
+            pocketLang.localize("message3", "Axel") shouldBe "Hallo Axel"
+        }
     }
 })
