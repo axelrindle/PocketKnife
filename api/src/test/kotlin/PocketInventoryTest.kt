77f8b4238737bebb83612c4b95d4152217fca50f
Axel Rindle <mail@axelrindle.de>
2020-06-30 17:24:40 +0200

    Remove top-level context

--- api/src/test/kotlin/PocketInventoryTest.kt
+++ api/src/test/kotlin/PocketInventoryTest.kt
@@ -16,63 +16,59 @@ class PocketInventoryTest : ShouldSpec({
     val mockedPlugin = MockBukkit.createMockPlugin()
     val player = MockBukkit.getMock().getPlayer(0)
 
-    context("PocketInventory") {
-
-        context("instantiation") {
-            should("succeed with size of 4") {
-                PocketInventory(mockedPlugin, "MyInventory", 4)
+    context("instantiation") {
+        should("succeed with size of 4") {
+            PocketInventory(mockedPlugin, "MyInventory", 4)
+        }
+        should("fail with size of 7") {
+            shouldThrow<IllegalArgumentException> {
+                PocketInventory(mockedPlugin, "MyInventory", 7)
             }
-            should("fail with size of 7") {
-                shouldThrow<IllegalArgumentException> {
-                    PocketInventory(mockedPlugin, "MyInventory", 7)
-                }
+        }
+    }
+
+    val pocketInventory = PocketInventory(mockedPlugin, "MyInventory", 6)
+    val pane = InventoryUtils.makeStack(Material.BLUE_STAINED_GLASS_PANE, "-")
+    for (x in 1..9) {
+        for (y in 1..6) {
+            if (x == 1 || x == 9 || y == 1 || y == 6) {
+                val index = InventoryUtils.getIndex(x, y)
+                pocketInventory.setItem(index, pane)
+            }
+        }
+    }
+
+    context("setItem") {
+        should("fail with negative position") {
+            shouldThrow<IllegalArgumentException> {
+                pocketInventory.setItem(-4, ItemStack(Material.APPLE))
             }
         }
 
-        val pocketInventory = PocketInventory(mockedPlugin, "MyInventory", 6)
-        val pane = InventoryUtils.makeStack(Material.BLUE_STAINED_GLASS_PANE, "-")
-        for (x in 1..9) {
-            for (y in 1..6) {
-                if (x == 1 || x == 9 || y == 1 || y == 6) {
-                    val index = InventoryUtils.getIndex(x, y)
-                    pocketInventory.setItem(index, pane)
-                }
-            }
+        should("succeed with valid position") {
+            val stack = ItemStack(Material.ENDER_EYE)
+            val pos = InventoryUtils.getIndex(5, 3)
+            pocketInventory.setItem(pos, stack)
+            pocketInventory.getItem(pos) shouldBeSameInstanceAs stack
         }
+    }
 
-        context("setItem") {
-            should("fail with negative position") {
-                shouldThrow<IllegalArgumentException> {
-                    pocketInventory.setItem(-4, ItemStack(Material.APPLE))
-                }
+    context("open") {
+        should("show the inventory and correctly set all items") {
+            pocketInventory.open(player) {
+                it.getItem(InventoryUtils.getIndex(5, 3)) shouldNotBe null
+                it.getItem(InventoryUtils.getIndex(5, 3))?.type shouldBe Material.ENDER_EYE
+                it.getItem(0)?.type shouldBe Material.BLUE_STAINED_GLASS_PANE
             }
-
-            should("succeed with valid position") {
-                val stack = ItemStack(Material.ENDER_EYE)
-                val pos = InventoryUtils.getIndex(5, 3)
-                pocketInventory.setItem(pos, stack)
-                pocketInventory.getItem(pos) shouldBeSameInstanceAs stack
-            }
+            player.openInventory.title shouldBe pocketInventory.name
+            player.openInventory.type shouldBe InventoryType.CHEST
         }
+    }
 
-        context("open") {
-            should("show the inventory and correctly set all items") {
-                pocketInventory.open(player) {
-                    it.getItem(InventoryUtils.getIndex(5, 3)) shouldNotBe null
-                    it.getItem(InventoryUtils.getIndex(5, 3))?.type shouldBe Material.ENDER_EYE
-                    it.getItem(0)?.type shouldBe Material.BLUE_STAINED_GLASS_PANE
-                }
-                player.openInventory.title shouldBe pocketInventory.name
-                player.openInventory.type shouldBe InventoryType.CHEST
-            }
+    context("removeItem") {
+        should("remove an item from the inventory") {
+            pocketInventory.removeItem(0)
+            pocketInventory.getItem(0) shouldBe null
         }
-
-        context("removeItem") {
-            should("remove an item from the inventory") {
-                pocketInventory.removeItem(0)
-                pocketInventory.getItem(0) shouldBe null
-            }
-        }
-
     }
 })
