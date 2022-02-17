607db34eee0d9b12ec72f8af71060d929102a74d
Axel Rindle <mail@axelrindle.de>
2022-02-17 20:21:53 +0100

    Update dependencies

--- api/src/test/kotlin/UtilClasses.kt
+++ api/src/test/kotlin/UtilClasses.kt
@@ -1,5 +1,11 @@
 import be.seeseemelk.mockbukkit.ServerMock
 import be.seeseemelk.mockbukkit.entity.PlayerMock
+import org.bukkit.GameMode
+import org.bukkit.Sound
+import org.bukkit.SoundCategory
+import org.bukkit.entity.Entity
+import org.bukkit.entity.SpawnCategory
+import org.bukkit.profile.PlayerProfile
 import java.util.*
 
 class CustomPlayerMock(
@@ -16,6 +22,26 @@ class CustomPlayerMock(
 
     fun setLocale(locale: String) {
         _locale = locale
+    }
+
+    override fun getSpawnCategory(): SpawnCategory {
+        return SpawnCategory.MISC
+    }
+
+    override fun getPlayerProfile(): PlayerProfile {
+        TODO("Not yet implemented")
+    }
+
+    override fun playSound(entity: Entity, sound: Sound, volume: Float, pitch: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun playSound(entity: Entity, sound: Sound, category: SoundCategory, volume: Float, pitch: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPreviousGameMode(): GameMode? {
+        TODO("Not yet implemented")
     }
 
 }
